5badcc287b07790ce86cc0a9c152e2ba9702e92f
Theo <b02241@163.com>
2024-04-09 13:17:28 +0800

    flask编写主成分分析提供api

--- python/pca/app.py
+++ python/pca/app.py
@@ -11,129 +11,152 @@ from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 import joblib
 import os
-
-app = Flask(__name__)
-
-# 设置全局字体为系统内置字体
-plt.rcParams['font.family'] = 'Microsoft YaHei'  # 指定字体为微软雅黑,可以根据自己的系统和字体库选择合适的字体
-plt.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
-
-if not os.path.exists('random_forest_model.pkl') or not os.path.exists('预测结果.csv'):
-    logger.logger.info('重新训练模型')
-
-    # 读取数据
-    df = pd.read_csv('【东软集团A08】医保特征数据16000（修订版2）.csv')
-    # 使用SimpleImputer填充缺失值
-    imputer = SimpleImputer(strategy='mean')
-    df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
-
-    # 数据预处理（根据需要进行填充、转换等）
-    X = df.drop(['RES'], axis=1)
-    feature_names = X.columns
-    y = df['RES']
-
-    # 标准化数据
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
-
-    # PCA降维
-    pca = PCA(n_components=0.95)  # 保留95%的方差
-    X_pca = pca.fit_transform(X_scaled)
-
-    # 划分数据集
-    X_train, X_test, y_train, y_test = train_test_split(X_pca, y, test_size=0.2, random_state=42)
-
-    # 建立随机森林模型并训练
-    model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train)
-
-    # 保存模型和训练数据以及特征列名称
-    model_data = {
-        'X': X,
-        'model': model,
-        'X_train': X_train,
-        'X_test': X_test,
-        'y_train': y_train,
-        'y_test': y_test,
-        'X_pca': X_pca,
-        'scaler': scaler,
-        'pca': pca
-    }
-    joblib.dump(model_data, 'random_forest_model.pkl')
-    model = model_data['model']
-    # 对每一条数据进行预测并判断是否骗保
-    predictions = []
-    print('正在处理预测')
-    for _, row in df.iterrows():
-        # 对单条数据进行预处理
-        row_data = pd.DataFrame(row.drop(['RES'])).T
-        row_data.columns = feature_names
-        row_data_scaled = scaler.transform(row_data)
-        row_data_pca = pca.transform(row_data_scaled)
-
-        # 进行预测
-        prediction = model.predict(row_data_pca)
-        predictions.append(prediction[0])
-
-    # 将预测结果添加到原始数据集中
-    df['预测是否骗保'] = predictions
-
-    # 将结果保存到CSV文件
-    df.to_csv('预测结果.csv', index=False)
-    print('处理预测完毕，已保存到预测结果.csv中')
-
-else:
-    # 加载模型和训练数据以及特征列名称
-    logger.logger.info('加载训练模型')
-    model_data = joblib.load('random_forest_model.pkl')
-    model = model_data['model']
-    X_train = model_data['X_train']
-    X_test = model_data['X_test']
-    y_train = model_data['y_train']
-    y_test = model_data['y_test']
-    X = model_data['X']
-    feature_names = X.columns
-    X_pca = model_data['X_pca']
-    scaler = model_data['scaler']
-    pca = model_data['pca']
-
-
-@app.route('/predict')
-def predict():
-    # 模型评估
-    y_pred = model.predict(X_test)
-    print("混淆矩阵:\n", confusion_matrix(y_test, y_pred))
-    print("\n分类报告:\n", classification_report(y_test, y_pred))
-
-    # 特征重要性分析
-    pca_feature_names = [f"PC{i + 1}" for i in range(X_pca.shape[1])]
-    feature_names_mapping = dict(zip(pca_feature_names, feature_names))
-    feature_importances = pd.Series(model.feature_importances_, index=pca_feature_names)
-    feature_importances.sort_values(ascending=False, inplace=True)
-
-    plt.figure(figsize=(20, 24))  # 调整图的大小
-    sns.barplot(x=feature_importances, y=feature_importances.index.map(feature_names_mapping))
-    plt.title("特征重要性（经PCA处理后）")
-    plt.savefig('feature_importance_plot.png')  # 保存图表为PNG文件
-
-    # 输出特征重要性数值
-    print("特征重要性:")
-    print(feature_importances)
-
-    # 将特征重要性数值转换为JSON格式
-    feature_importances_json = feature_importances.to_json()
-    return jsonify(feature_importances_json)
-
-
-@app.route('/download_csv')
-def download_csv():
-    # 本地CSV文件路径
-    csv_path = '预测结果.csv'
-    try:
-        return send_file(csv_path, as_attachment=True, download_name='file.csv')
-    except Exception as e:
-        return str(e)
-
+#
+# app = Flask(__name__)
+#
+# # 设置全局字体为系统内置字体
+# plt.rcParams['font.family'] = 'Microsoft YaHei'  # 指定字体为微软雅黑,可以根据自己的系统和字体库选择合适的字体
+# plt.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
+#
+# if not os.path.exists('random_forest_model.pkl') or not os.path.exists('预测结果.csv'):
+#     logger.logger.info('重新训练模型')
+#
+#     # 读取数据
+#     df = pd.read_csv('【东软集团A08】医保特征数据16000（修订版2）.csv')
+#     # 使用SimpleImputer填充缺失值
+#     imputer = SimpleImputer(strategy='mean')
+#     df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+#
+#     # 数据预处理（根据需要进行填充、转换等）
+#     X = df.drop(['RES'], axis=1)
+#     feature_names = X.columns
+#     y = df['RES']
+#
+#     # 标准化数据
+#     scaler = StandardScaler()
+#     X_scaled = scaler.fit_transform(X)
+#
+#     # PCA降维
+#     pca = PCA(n_components=0.95)  # 保留95%的方差
+#     X_pca = pca.fit_transform(X_scaled)
+#
+#     # 划分数据集
+#     X_train, X_test, y_train, y_test = train_test_split(X_pca, y, test_size=0.2, random_state=42)
+#
+#     # 建立随机森林模型并训练
+#     model = RandomForestClassifier(random_state=42)
+#     model.fit(X_train, y_train)
+#
+#     # 保存模型和训练数据以及特征列名称
+#     model_data = {
+#         'X': X,
+#         'model': model,
+#         'X_train': X_train,
+#         'X_test': X_test,
+#         'y_train': y_train,
+#         'y_test': y_test,
+#         'X_pca': X_pca,
+#         'scaler': scaler,
+#         'pca': pca
+#     }
+#     joblib.dump(model_data, 'random_forest_model.pkl')
+#     model = model_data['model']
+#     # 对每一条数据进行预测并判断是否骗保
+#     predictions = []
+#     print('正在处理预测')
+#     for _, row in df.iterrows():
+#         # 对单条数据进行预处理
+#         row_data = pd.DataFrame(row.drop(['RES'])).T
+#         row_data.columns = feature_names
+#         row_data_scaled = scaler.transform(row_data)
+#         row_data_pca = pca.transform(row_data_scaled)
+#
+#         # 进行预测
+#         prediction = model.predict(row_data_pca)
+#         predictions.append(prediction[0])
+#
+#     # 将预测结果添加到原始数据集中
+#     df['预测是否骗保'] = predictions
+#
+#     # 将结果保存到CSV文件
+#     df.to_csv('预测结果.csv', index=False)
+#     print('处理预测完毕，已保存到预测结果.csv中')
+#
+# else:
+#     # 加载模型和训练数据以及特征列名称
+#     logger.logger.info('加载训练模型')
+#     model_data = joblib.load('random_forest_model.pkl')
+#     model = model_data['model']
+#     X_train = model_data['X_train']
+#     X_test = model_data['X_test']
+#     y_train = model_data['y_train']
+#     y_test = model_data['y_test']
+#     X = model_data['X']
+#     feature_names = X.columns
+#     X_pca = model_data['X_pca']
+#     scaler = model_data['scaler']
+#     pca = model_data['pca']
+#
+#
+# @app.route('/predict')
+# def predict():
+#     # 模型评估
+#     y_pred = model.predict(X_test)
+#     print("混淆矩阵:\n", confusion_matrix(y_test, y_pred))
+#     print("\n分类报告:\n", classification_report(y_test, y_pred))
+#
+#     # 特征重要性分析
+#     pca_feature_names = [f"PC{i + 1}" for i in range(X_pca.shape[1])]
+#     feature_names_mapping = dict(zip(pca_feature_names, feature_names))
+#     feature_importances = pd.Series(model.feature_importances_, index=pca_feature_names)
+#     feature_importances.sort_values(ascending=False, inplace=True)
+#
+#     plt.figure(figsize=(20, 24))  # 调整图的大小
+#     sns.barplot(x=feature_importances, y=feature_importances.index.map(feature_names_mapping))
+#     plt.title("特征重要性（经PCA处理后）")
+#     plt.savefig('feature_importance_plot.png')  # 保存图表为PNG文件
+#
+#     # 输出特征重要性数值
+#     print("特征重要性:")
+#     print(feature_importances)
+#
+#     # 将特征重要性数值转换为JSON格式
+#     feature_importances_json = feature_importances.to_json()
+#     return jsonify(feature_importances_json)
+#
+#
+# @app.route('/download_csv')
+# def download_csv():
+#     # 本地CSV文件路径
+#     csv_path = '预测结果.csv'
+#     try:
+#         return send_file(csv_path, as_attachment=True, download_name='file.csv')
+#     except Exception as e:
+#         return str(e)
+#
+import json
 
 if __name__ == '__main__':
-    app.run()
+    data = {
+        "code": 200,
+        "data": {
+            "feature_importance": "[{\"Feature\":\"num__月统筹金额_MAX\",\"Importance\":0.0513178432},{\"Feature\":\"num__ALL_SUM\",\"Importance\":0.0425410725},{\"Feature\":\"num__月药品金额_MAX\",\"Importance\":0.0420588128},{\"Feature\":\"num__顺序号_NN\",\"Importance\":0.0366992805},{\"Feature\":\"num__本次审批金额_SUM\",\"Importance\":0.0327076953},{\"Feature\":\"num__月药品金额_AVG\",\"Importance\":0.0277709245},{\"Feature\":\"num__月就诊次数_MAX\",\"Importance\":0.0257374899},{\"Feature\":\"num__药品费发生金额_SUM\",\"Importance\":0.0254232779},{\"Feature\":\"num__月统筹金额_AVG\",\"Importance\":0.0248570204},{\"Feature\":\"num__月就诊天数_AVG\",\"Importance\":0.0247316163},{\"Feature\":\"num__统筹支付金额_SUM\",\"Importance\":0.0246634855},{\"Feature\":\"num__基本统筹基金支付金额_SUM\",\"Importance\":0.023662617},{\"Feature\":\"num__月就诊天数_MAX\",\"Importance\":0.0231492142},{\"Feature\":\"num__月就诊次数_AVG\",\"Importance\":0.0227158646},{\"Feature\":\"num__药品费申报金额_SUM\",\"Importance\":0.0226657328},{\"Feature\":\"num__起付标准以上自负比例金额_SUM\",\"Importance\":0.020925449},{\"Feature\":\"num__就诊次数_SUM\",\"Importance\":0.0194006155},{\"Feature\":\"num__交易时间DD_NN\",\"Importance\":0.019114859},{\"Feature\":\"num__治疗费申报金额_SUM\",\"Importance\":0.0189241326},{\"Feature\":\"num__医院_统筹金_AVG\",\"Importance\":0.0177140412},{\"Feature\":\"num__医院_统筹金_MAX\",\"Importance\":0.0173963727},{\"Feature\":\"num__中成药费发生金额_SUM\",\"Importance\":0.0171903793},{\"Feature\":\"num__药品在总金额中的占比\",\"Importance\":0.0170212899},{\"Feature\":\"num__医院_药品_AVG\",\"Importance\":0.016791049},{\"Feature\":\"num__治疗费用在总金额占比\",\"Importance\":0.0165858015},{\"Feature\":\"num__个人账户金额_SUM\",\"Importance\":0.0165065319},{\"Feature\":\"num__非账户支付金额_SUM\",\"Importance\":0.0163514537},{\"Feature\":\"num__治疗费发生金额_SUM\",\"Importance\":0.0162092849},{\"Feature\":\"num__可用账户报销金额_SUM\",\"Importance\":0.0155279193},{\"Feature\":\"num__医院_药品_MAX\",\"Importance\":0.0154320911},{\"Feature\":\"num__药品费自费金额_SUM\",\"Importance\":0.0153274533},{\"Feature\":\"num__医院_就诊天数_AVG\",\"Importance\":0.0152437916},{\"Feature\":\"num__出院诊断LENTH_MAX\",\"Importance\":0.0149846646},{\"Feature\":\"num__医院_就诊天数_MAX\",\"Importance\":0.0144886828},{\"Feature\":\"num__医用材料发生金额_SUM\",\"Importance\":0.0136733553},{\"Feature\":\"num__个人支付的药品占比\",\"Importance\":0.013496025},{\"Feature\":\"num__检查费申报金额_SUM\",\"Importance\":0.0126727815},{\"Feature\":\"num__检查费发生金额_SUM\",\"Importance\":0.0125548922},{\"Feature\":\"num__一次性医用材料申报金额_SUM\",\"Importance\":0.0118860245},{\"Feature\":\"num__出院诊断病种名称_NN\",\"Importance\":0.0115279373},{\"Feature\":\"num__检查总费用在总金额占比\",\"Importance\":0.0111720835},{\"Feature\":\"num__月就诊医院数_AVG\",\"Importance\":0.011081175},{\"Feature\":\"num__基本个人账户支付_SUM\",\"Importance\":0.0110715855},{\"Feature\":\"num__贵重药品发生金额_SUM\",\"Importance\":0.0107325567},{\"Feature\":\"num__中草药费发生金额_SUM\",\"Importance\":0.0105338914},{\"Feature\":\"num__交易时间YYYYMM_NN\",\"Importance\":0.0076256889},{\"Feature\":\"num__就诊的月数\",\"Importance\":0.0074679179},{\"Feature\":\"num__医疗救助个人按比例负担金额_SUM\",\"Importance\":0.0065731106},{\"Feature\":\"num__一天去两家医院的天数\",\"Importance\":0.0064166401},{\"Feature\":\"num__医用材料费自费金额_SUM\",\"Importance\":0.0062839564},{\"Feature\":\"num__医疗救助医院申请_SUM\",\"Importance\":0.0058502052},{\"Feature\":\"num__医院编码_NN\",\"Importance\":0.0055657555},{\"Feature\":\"num__月就诊医院数_MAX\",\"Importance\":0.0055576516},{\"Feature\":\"num__补助审批金额_SUM\",\"Importance\":0.0053795631},{\"Feature\":\"num__其它发生金额_SUM\",\"Importance\":0.0051365624},{\"Feature\":\"num__民政救助补助_SUM\",\"Importance\":0.0045275104},{\"Feature\":\"num__城乡救助补助金额_SUM\",\"Importance\":0.0041007615},{\"Feature\":\"num__治疗费自费金额_SUM\",\"Importance\":0.0036922183},{\"Feature\":\"num__个人支付治疗费用占比\",\"Importance\":0.0032816812},{\"Feature\":\"num__检查费自费金额_SUM\",\"Importance\":0.0032437269},{\"Feature\":\"num__个人支付检查费用占比\",\"Importance\":0.0029887058},{\"Feature\":\"num__床位费发生金额_SUM\",\"Importance\":0.002363317},{\"Feature\":\"num__贵重检查费金额_SUM\",\"Importance\":0.0023301563},{\"Feature\":\"num__床位费申报金额_SUM\",\"Importance\":0.0022634927},{\"Feature\":\"num__是否挂号\",\"Importance\":0.0017334028},{\"Feature\":\"num__公务员医疗补助基金支付金额_SUM\",\"Importance\":0.0014864709},{\"Feature\":\"num__高价材料发生金额_SUM\",\"Importance\":0.0011633849},{\"Feature\":\"num__BZ_民政救助\",\"Importance\":0.0010623866},{\"Feature\":\"num__起付线标准金额_MAX\",\"Importance\":0.0005968232},{\"Feature\":\"num__城乡优抚补助_SUM\",\"Importance\":0.0002780295},{\"Feature\":\"num__手术费自费金额_SUM\",\"Importance\":0.0002090526},{\"Feature\":\"num__BZ_城乡优抚\",\"Importance\":0.0002037163},{\"Feature\":\"num__手术费发生金额_SUM\",\"Importance\":0.0001521876},{\"Feature\":\"num__手术费申报金额_SUM\",\"Importance\":0.0001466359},{\"Feature\":\"num__其它申报金额_SUM\",\"Importance\":0.0000480253},{\"Feature\":\"num__成分输血申报金额_SUM\",\"Importance\":0.0000227396},{\"Feature\":\"num__残疾军人补助_SUM\",\"Importance\":0.0000084046},{\"Feature\":\"num__住院天数_SUM\",\"Importance\":0.0},{\"Feature\":\"num__交易时间YYYY_NN\",\"Importance\":0.0},{\"Feature\":\"num__最高限额以上金额_SUM\",\"Importance\":0.0}]"
+        },
+        "message": "success"
+    }
+
+    # 解析JSON数据
+    # data = json.loads(json_string)
+    feature_importance = json.loads(data['data']['feature_importance'])
+
+    # 提取贡献度值
+    importance_values = [float(feature['Importance']) for feature in feature_importance]
+
+    # 计算贡献度的平均值
+    average_importance = sum(importance_values) / len(importance_values)
+
+    # 统计贡献度高于平均值的特征数量
+    count_above_average = sum(1 for value in importance_values if value > average_importance)
+
+    print(f"贡献度均值: {average_importance:.6f}")
+    print(f"贡献度高于平均值的特征数量: {count_above_average}")
--- python/pca/new/app.py
+++ python/pca/new/app.py
@@ -1,3 +1,4 @@
+import re
 import shutil
 
 from flask import render_template, jsonify, Flask, request, send_file
@@ -15,30 +16,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import seaborn as sns
+from flask_cors import CORS
+app = Flask(__name__,
+            static_folder='./static',  # 设置静态文件夹目录
+            template_folder="./static",
+            static_url_path="")  # 设置vue编译输出目录dist文件夹，为Flask模板文件目录
 
-# 配置日志记录
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+CORS(app, resources=r'/*')
+app.config['TEMPLATES_FOLDER'] = 'templates'
 
-app = Flask(__name__, template_folder=os.path.dirname(__file__))
-model_dir = './model'
 
 # 如果你没有使用 Matplotlib 或 Seaborn 则以下两行可以注释掉
-plt.rcParams['font.family'] = 'Microsoft YaHei'
-plt.rcParams['axes.unicode_minus'] = False
+# plt.rcParams['font.family'] = 'Microsoft YaHei'
+# plt.rcParams['axes.unicode_minus'] = False
 
-
-# 文件上传处理
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/api/upload', methods=['GET', 'POST'])
 def upload():
-    # 检查文件是否在请求中，并且提供了模型名称
+    # 检查文件是否在请求中
     if 'file' not in request.files:
         return jsonify({'code': 400, 'message': '没有上传文件'})
-
     file = request.files['file']
-    model_name = request.form.get('model_name')
-
-    if not model_name:
-        return jsonify({'code': 400, 'message': '未指定模型名称'})
+    # 如果未提供model_name，则默认为"医疗保险欺诈识别监测模型1"
+    model_name = request.form.get('model_name', '医疗保险欺诈识别监测模型1') or request.args.get('model_name',
+                                                                                                 '医疗保险欺诈识别监测模型1')
 
     # 文件检验
     if not file.filename:
@@ -54,8 +55,10 @@ def upload():
 
     # 文件处理与反馈
     try:
-        df = pd.read_excel(file_path) if file_path.lower().endswith('.xlsx') else pd.read_csv(file_path,
-                                                                                              encoding='utf-8')
+        if file_path.lower().endswith('.xlsx'):
+            df = pd.read_excel(file_path)
+        else:
+            df = pd.read_csv(file_path, encoding='utf-8')
         return jsonify({
             'code': 200,
             'message': 'success',
@@ -66,10 +69,11 @@ def upload():
             }
         })
     except Exception as e:
-        return jsonify({'code': 500, 'message': '读取文件时发生错误: ' + str(e)})
+        return jsonify({'code': 500, 'message': f'读取文件时发生错误: {e}'})
 
 
-@app.route('/process_missing_value')
+@app.route('/api/process_missing_value', methods=['GET', 'POST'])
+@app.route('/process_missing_value', methods=['GET', 'POST'])
 def process_missing_value():
     file_path = request.form.get("file_path") or request.args.get("file_path")
     missing_value = request.form.get("missing_value") or request.args.get("missing_value")
@@ -137,38 +141,46 @@ def process_missing_value():
     })
 
 
-# 主页路由，返回主页面
-@app.route('/')
-def home():
-    # 注意render_template函数需要返回index.html文件，所以确保templates文件夹中有该文件
-    return render_template('index.html')
-
-
 # 新建模型的路由，创建模型所需的文件夹结构
-@app.route('/new_model')
+@app.route('/api/new_model', methods=['GET', 'POST'])
+@app.route('/new_model', methods=['GET', 'POST'])
 def new_model():
     model_name = request.args.get("model_name")
+    os.makedirs(model_dir, exist_ok=True)  # 确保模型根目录存在
+    model_list = [name for name in os.listdir(model_dir) if os.path.isdir(os.path.join(model_dir, name))]
+
     if not model_name:
-        return jsonify({'code': 400, 'message': '未指定模型名称'})
+        # 匹配模型名称中的数字，找到最大的编号
+        pattern = r"医疗保险欺诈识别监测模型(\d+)"
+        max_num = 0
+        for name in model_list:
+            match = re.match(pattern, name)
+            if match:
+                num = int(match.group(1))
+                if num > max_num:
+                    max_num = num
+        # 生成新的模型名称
+        model_name = f"医疗保险欺诈识别监测模型{max_num + 1}"
 
     # 创建模型的相关目录
     new_model_dir = os.path.join(model_dir, model_name)
     os.makedirs(os.path.join(new_model_dir, 'uploads'), exist_ok=True)
     os.makedirs(os.path.join(new_model_dir, 'results'), exist_ok=True)
 
-    return jsonify({'code': 200, 'message': '新模型文件夹创建成功'})
+    return jsonify({'code': 200, 'message': '新模型文件夹创建成功', 'model_name': model_name})
 
 
-@app.route('/process_file')
+@app.route('/api/process_file', methods=['GET', 'POST'])  # 支持GET和POST请求
+@app.route('/process_file', methods=['GET', 'POST'])  # 支持GET和POST请求
 def process_file():
     # 获取请求参数
     file_path = request.form.get("file_path") or request.args.get("file_path")
     model_name = request.form.get("model_name") or request.args.get("model_name")
+
     # 验证参数
     if not file_path or not model_name:
         return jsonify({'code': 400, 'message': '路径、文件名称或模型名称错误'})
 
-    # 构建模型文件路径并加载模型
     logging.info('构建模型文件路径并加载模型')
     model_file_path = os.path.join('./model', model_name, 'fraud_detection_model.pkl')
     if not os.path.exists(model_file_path):
@@ -185,32 +197,29 @@ def process_file():
     else:
         return jsonify({'code': 400, 'message': '测试文件类型错误'})
 
-    # 预处理测试数据并进行预测
     logging.info('预处理测试数据并进行预测')
-    if '个人编码' in df_test.columns:
-        df_test.drop(columns=['个人编码'], inplace=True)
-    if 'RES' in df_test.columns:
-        X_test = df_test.drop(columns=['RES'])
-    else:
-        X_test = df_test
+    # 保留个人编码和RES列，如果存在
+    X_test = df_test.drop(columns=['RES'], errors='ignore')
     y_pred = model.predict(X_test)
-    df_test['预测是否欺诈'] = y_pred
+    df_test['预测是否欺诈'] = y_pred  # 添加预测结果到新列
 
     # 保存结果文件
     logging.info('保存结果文件')
     results_dir = os.path.join('./model', model_name, 'results')
     os.makedirs(results_dir, exist_ok=True)
-    result_file_path = os.path.join(results_dir, 'result.xlsx')
+    result_file_path = os.path.join(results_dir, 'processed_result.xlsx')
     df_test.to_excel(result_file_path, index=False)
 
     # 返回文件供下载
     try:
-        return send_file(result_file_path, as_attachment=True)
+        return send_file(result_file_path, as_attachment=True, download_name='processed_result.xlsx')
     except Exception as e:
+        logging.error(f'文件发送失败: {str(e)}')
         return jsonify({'code': 500, 'message': f'文件发送失败: {str(e)}'})
 
 
-@app.route('/run_model', methods=['POST'])
+@app.route('/api/run_model', methods=['GET', 'POST'])
+@app.route('/run_model', methods=['GET', 'POST'])
 def run_model():
     logging.info('正在加载模型')
     """
@@ -218,18 +227,22 @@ def run_model():
     """
     # 获取请求参数
     file_path = request.form.get("file_path") or request.args.get("file_path")
-    model_path = request.form.get("model_path") or request.args.get("model_path")
+    model_name = request.form.get("model_name") or request.args.get("model_name")
     random_state = int(request.form.get("random_state", 42))
     test_size = float(request.form.get("test_size", 0.2))
     missing_value_strategy = request.form.get("missing_value", "mean")
 
+    logging.info(file_path)
     # 验证文件类型并读取数据
-    if file_path.endswith('.xlsx'):
+    if str(file_path).endswith('.xlsx'):
         df = pd.read_excel(file_path)
-    elif file_path.endswith('.csv'):
+    elif str(file_path).endswith('.csv'):
         df = pd.read_csv(file_path, encoding='utf-8')
     else:
         return jsonify({'code': 400, 'message': '文件类型错误'})
+
+    if model_name is None:
+        return jsonify({'code': 400, 'message': '模型路径不能为空'})
 
     # 处理缺失值
     logging.info('处理缺失值')
@@ -272,8 +285,8 @@ def run_model():
 
     # 保存模型
     logging.info('保存模型')
-    os.makedirs(model_path, exist_ok=True)
-    joblib.dump(model, os.path.join(model_path, 'fraud_detection_model.pkl'))
+    os.makedirs(model_name, exist_ok=True)
+    joblib.dump(model, os.path.join(model_name, 'fraud_detection_model.pkl'))
 
     # 评估模型
     logging.info('进行模型评估')
@@ -282,6 +295,7 @@ def run_model():
     report = classification_report(y_test, y_pred, output_dict=True)
     feature_importances = model.named_steps['classifier'].feature_importances_
     cm = confusion_matrix(y_test, y_pred)
+    # cm = confusion_matrix(y_test, y_pred, labels=[0, 1])
     y_pred_proba = model.predict_proba(X_test)[:, 1]
     fpr, tpr, _ = roc_curve(y_test, y_pred_proba)
     roc_auc = auc(fpr, tpr)
@@ -302,14 +316,16 @@ def run_model():
     return jsonify({'code': 200, 'message': '模型运行成功', 'data': result})
 
 
-@app.route('/model_list')
+@app.route('/api/model_list', methods=['GET', 'POST'])
+@app.route('/model_list', methods=['GET', 'POST'])
 def chose_model():
     os.makedirs(model_dir, exist_ok=True)
     model_list = [name for name in os.listdir(model_dir) if os.path.isdir(os.path.join(model_dir, name))]
     return jsonify(model_list)
 
 
-@app.route("/delete_model", methods=["POST"])
+@app.route("/api/delete_model", methods=['GET', 'POST'])
+@app.route("/delete_model", methods=['GET', 'POST'])
 def delete_model():
     model_name = request.form.get("model_name") or request.args.get("model_name")
 
@@ -329,9 +345,11 @@ def delete_model():
 
 
 def run_default_model(path):
+    plt.rcParams['font.family'] = 'Microsoft YaHei'
+    plt.rcParams['axes.unicode_minus'] = False
     # 第1步:加载数据
     logging.info('加载数据')
-    df = pd.read_csv('./【东软集团A08】医保特征数据16000（修订版）.csv', encoding='utf-8')
+    df = pd.read_csv('./static/【东软集团A08】医保特征数据16000（修订版）.csv', encoding='utf-8')
     imputer = SimpleImputer(strategy='mean')
     df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
     # 第2步:数据探索
@@ -374,8 +392,14 @@ def run_default_model(path):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
+    # 调整分类阈值以提高召回率
+    # 评估模型
     print("准确率:", accuracy_score(y_test, y_pred))
     print("分类报告:\n", classification_report(y_test, y_pred))
+    # # 调整分类阈值以提高召回率
+    # y_pred_proba = model.predict_proba(X_test)[:, 1]
+    # y_pred_new = (y_pred_proba >= 0.4).astype(int)
+    # print("调整分类阈值后的分类报告:\n", classification_report(y_test, y_pred_new))
     # 生成分类报告
     report = classification_report(y_test, y_pred, output_dict=True)
     # 将分类报告转换为DataFrame
@@ -396,19 +420,34 @@ def run_default_model(path):
     # 将图形保存为PNG文件
     plt.savefig('{}/classification_report_table.png'.format(path))
     # 显示图形(可选)
-    plt.show()
+    # plt.show()
     # 绘制特征重要性图
+    # 假设preprocessor是你的ColumnTransformer实例
+    # 假设selector是SelectFromModel实例，通过它进行了特征选择
+    # 注意：如果你的pipeline没有使用SelectFromModel进行特征选择，那么下面的selector相关代码可以省略
+
+    # 获取通过ColumnTransformer预处理后的特征名称
+    preprocessed_feature_names = preprocessor.get_feature_names_out()
+
+    # 如果使用了特征选择，进一步筛选被选中的特征
+    if 'selector' in model.named_steps:
+        selected_mask = model.named_steps['selector'].get_support()
+        selected_feature_names = preprocessed_feature_names[selected_mask]
+    else:
+        selected_feature_names = preprocessed_feature_names
+
+    # 获取特征重要性并与选中的特征名称匹配
     feature_importances = model.named_steps['classifier'].feature_importances_
-    feature_names = X.columns
-    plt.figure(figsize=(10, 15))
+    plt.figure(figsize=(10, 10))  # 根据特征数量调整图的高度
     plt.barh(range(len(feature_importances)), feature_importances, align='center')
-    plt.yticks(range(len(feature_names)), feature_names)
+    plt.yticks(range(len(selected_feature_names)), selected_feature_names)
     plt.xlabel('特征重要性')
     plt.ylabel('特征')
     plt.title('特征重要性图')
     plt.tight_layout()
     plt.savefig('{}/feature_importance.png'.format(path))
-    plt.show()
+    # plt.show()
+
     # 绘制混淆矩阵
     cm = confusion_matrix(y_test, y_pred)
     plt.figure(figsize=(8, 6))
@@ -417,7 +456,7 @@ def run_default_model(path):
     plt.ylabel('真实标签')
     plt.title('混淆矩阵')
     plt.savefig('{}/confusion_matrix.png'.format(path))
-    plt.show()
+    # plt.show()
     # 绘制ROC曲线和AUC值
     y_pred_proba = model.predict_proba(X_test)[:, 1]
     fpr, tpr, thresholds = roc_curve(y_test, y_pred_proba)
@@ -430,23 +469,28 @@ def run_default_model(path):
     plt.title('ROC曲线')
     plt.legend(loc='lower right')
     plt.savefig('{}/roc_curve.png'.format(path))
-    plt.show()
+    # plt.show()
     # 第8步:模型保存
     logging.info('第8步:模型保存')
     joblib.dump(model, '{}/fraud_detection_model.pkl'.format(path))
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+model_dir = './model'
 if __name__ == '__main__':
     # 检查并创建必要的目录
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
-    if not os.path.exists('{}/default'.format(model_dir)):
-        os.makedirs('{}/default'.format(model_dir))
-    if not os.path.exists('{}/default/fraud_detection_model.pkl'.format(model_dir)):
-        run_default_model('{}/default'.format(model_dir))
-    if not os.path.exists('{}/default/uploads'.format(model_dir)):
-        os.makedirs('{}/default/uploads'.format(model_dir))
-    if not os.path.exists('{}/default/results'.format(model_dir)):
-        os.makedirs('{}/default/results'.format(model_dir))
-    # 运行 Flask 应用
-    app.run(host='0.0.0.0', port=5000)
+    if not os.path.exists('{}/医疗保险欺诈识别监测模型1'.format(model_dir)):
+        os.makedirs('{}/医疗保险欺诈识别监测模型1'.format(model_dir))
+    if not os.path.exists('{}/医疗保险欺诈识别监测模型1/fraud_detection_model.pkl'.format(model_dir)):
+        run_default_model('{}/医疗保险欺诈识别监测模型1'.format(model_dir))
+    if not os.path.exists('{}/医疗保险欺诈识别监测模型1/uploads'.format(model_dir)):
+        os.makedirs('{}/医疗保险欺诈识别监测模型1/uploads'.format(model_dir))
+    if not os.path.exists('{}/医疗保险欺诈识别监测模型1/results'.format(model_dir)):
+        os.makedirs('{}/医疗保险欺诈识别监测模型1/results'.format(model_dir))
+    app.run(host='0.0.0.0', port=5000, debug=True)
